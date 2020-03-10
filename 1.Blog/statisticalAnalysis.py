7c551ac322f1b27f47c6a30207e14f0188217aaa
davidson <davidson.faria@jovempan.com.br>
2020-03-10 17:37:34 -0300

    ADD: avgGrade, gradeHDIRelation-test

--- 1.Blog/statisticalAnalysis.py
+++ 1.Blog/statisticalAnalysis.py
@@ -2,32 +2,43 @@ import re
 import os
 import pandas as pd
 
-
-def meanTest(test):
+def meanTestFiltered(test):
+    """
+        Calculates only the average of the test of the students who take the test.
+        Parameters:
+            test: dataframe with presence and grade columns.
+        Returns:
+            dictionary with average, min and max grades.
+    """
     import re
     grades = []
+    presence = 0
     for i, r in test.iterrows():
-        if r[list(filter(lambda col: re.search("(presence|status)", col), test.columns))[0]] == 1.0:
+        if r[list(filter(lambda col: re.search("(presence|status)", col), test.columns))[0]] >= 1.0:
             grades.append(r[list(filter(lambda col: re.search("grade", col), test.columns))[0]])
-    return({ 'mean': sum(grades)/len(grades)
-           ,'min': min(grades)
-           ,'max': max(grades)
-         })
+            presence+=1
+    return({ 'presence': presence
+           , 'mean': sum(grades)/len(grades)
+           , 'min': min(grades)
+           , 'max': max(grades)
+           })
         
 
 def avgGrade(enem, onlyPresent=True):
     """
-        Returns average grade of each test
-        Attributes:
-            enem (pandas dataframe): ENEM dataframe that contains all grades
-            onlyPresent (boolean): TRUE if you only want the grades of the students who took the test. FALSE if you want all the grades
+        Returns average grade of each test.
+        Parameters:
+            enem (pandas dataframe): ENEM dataframe that contains all grades.
+            onlyPresent (boolean): TRUE if you only want the grades of the students who took the test. FALSE if you want all the grades.
+        Returns:
+            dictionary with test content and their respective averages, min e max grades.
     """
     if onlyPresent:
-        return { 'natural_science': meanTest(enem[['presence_natural_science', 'grade_human_science']])
-                ,'human_science': meanTest(enem[['presence_languages', 'grade_natural_science']])
-                ,'languages': meanTest(enem[['presence_human_science', 'grade_languages']])
-                ,'math': meanTest(enem[['presence_math', 'grade_math']])
-                ,'essay': meanTest(enem[['essay_status', 'grade_essay']])
+        return { 'natural_science': meanTestFiltered(enem[['presence_natural_science', 'grade_natural_science']])
+                ,'human_science': meanTestFiltered(enem[['presence_human_science', 'grade_human_science']])
+                ,'languages': meanTestFiltered(enem[['presence_languages', 'grade_languages']])
+                ,'math': meanTestFiltered(enem[['presence_math', 'grade_math']])
+                ,'essay': meanTestFiltered(enem[['essay_status', 'grade_essay']])
                } 
     else:
         return { 'natural_science': { 'mean': enem['grade_natural_science'].mean()
@@ -51,3 +62,9 @@ def avgGrade(enem, onlyPresent=True):
                            ,'max': enem['grade_essay'].max()
                           }
                }
+        
+# {'mean_grade_natural_science': []
+# ,'mean_grade_human_science': []
+# ,'mean_grade_languages': []
+# ,'mean_grade_math': []
+# ,'mean_grade_essay': []}
--- 1.Blog/utils.py
+++ 1.Blog/utils.py
@@ -4,10 +4,19 @@ import re
 import os
 import pandas as pd
 import json
+from pprint import pprint
+from tabulate import tabulate
 
-def print_json(res, indent=2):
-    json_object = json.loads(res)
-    print(json.dumps(json_object, indent=2))
-    
-def funcionaFuncao(res):
-    print('funciona')
+
+def print_pretty(res, indent=2, tabular=False):
+    if(tabular):
+        if type(res) == type({}) or type(res) == type([]):
+            dict_keys = list(res['essay'].keys())
+            print(tabulate([ [k, *v.values()] for k, v in res.items() ], headers=['content', *dict_keys]))
+        else:
+            print(json.dumps(res, indent, ensure_ascii=False))
+    else:
+        if type(res) == type({}) or type(res) == type([]):
+            pprint(res, indent=indent, width=1)
+        else:
+            print(json.dumps(res, indent, ensure_ascii=False))
